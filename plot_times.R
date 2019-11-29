d2fe01418134cf57269e769e9aa9f1c0c79a7dff
gherardovarando <gherardo.varando@gmail.com>
2019-11-29 08:04:08 +0100

    plot times

---
+++ plot_times.R
@@ -0,0 +1,51 @@
+source("functions/util.R")
+
+rep <- 100
+N <- 3000
+ks <- c(1,2,3,4)
+P <- 10
+p <- 10
+algsel <- c("loglik", "frobenius", "lasso", "glasso", "covthr") 
+
+load(paste0("simulations/results_p",p,".RData"))
+
+plotpath <- paste0("plot/simulations/" )
+avgrestable <- apply(restable[,,,,,], MARGIN = c(1,2,3,5), mean)
+df <- expand.grid(dimnames(avgrestable))
+df <- cbind(df,Y = (apply(df, 1, function(x){
+  avgrestable[x[1], x[2], x[3], x[4]]
+})))
+df1 <- df[df$stats %in% c("elapsed"),]
+df1 <- df1[df1$Algorithm %in% algsel, ]
+df1$k <- paste0("k=",df1$k)
+df1$N <- as.numeric(levels(df1$N))[as.numeric(df1$N)]
+
+ggplot(df1, aes(x = N, y = Y, group = Algorithm, 
+                color = Algorithm)) + 
+  facet_grid(cols = vars(k), 
+#             rows = vars(stats), 
+             scales = "free_y") + 
+  geom_path() + 
+#  geom_hline(data = df2, 
+#             aes(yintercept = Y), linetype = "dotted") +
+  theme_bw() + 
+  scale_x_log10() +
+  scale_y_continuous(limits = function(x){
+    x <- x + c(-0.0005, 0.0005) * x
+    x <- c(floor(x[1] * 10),
+           ceiling(x[2] * 10)) / 10
+    return(x)
+  }, breaks = function(x) {
+    seq(x[1], x[2], length.out = 11)[c(2,4,6,8,10)]
+  }, expand = expand_scale(0,0)) + 
+  theme(legend.position = "bottom", 
+#        axis.title.y = element_blank(),
+        axis.text.x = element_text(angle = 30)) + 
+        ylab("sec.") +  
+  ggsave(paste0("times_p", p, ".pdf"), path = plotpath,
+         width = 6, height = 2, units = "in")
+
+
+
+
+
